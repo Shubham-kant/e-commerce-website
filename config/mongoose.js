53fe067dd0ea8db03054c727fa39a2b2b8bd4205
Shubham-Kant <pro.shubham1999@gmail.com>
2020-12-21 20:48:21 +0530

    using mongo url from env if possible

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/e-commerce_development');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/e-commerce_development');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting mongodb'));
 db.once('open',function(){
