2e035e7ba4ac616296f63d1db7e8a37eac14529b
Shubham-Kant <pro.shubham1999@gmail.com>
2020-12-27 21:21:49 +0530

    debugging mongoose file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,13 @@
 const mongoose=require('mongoose');
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/e-commerce_development');
+// mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/e-commerce_development');
+mongoose
+    .connect(process.env.MONGO_URL || 'mongodb://localhost/e-commerce_development',{
+        useUnifiedTopology:true,
+        useNewUrlParser:true,
+        useCreateIndex:true,
+    })
+    .then(()=> console.log('MongoDB connected....'))
+    .catch((err) => console.log(err));
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting mongodb'));
 db.once('open',function(){
